588e61420cabfb7b8230496a353b7110e240daa9
Tiago Royer <royertiago@gmail.com>
2014-04-27 21:37:16 -0300

    Grammar::generate implementado; Exemplo em README corrigido.

--- grammar.cpp
+++ grammar.cpp
@@ -3,11 +3,14 @@
  */
 
 #include <set>
+#include <algorithm>
 #include "grammar.h"
 
 using std::set;
 using std::vector;
 using std::string;
+using std::count_if;
+using std::copy_if;
 
 Grammar::Grammar( string Vt, string s, vector<Production> p ) :
     terminals( Vt ),
@@ -17,4 +20,33 @@ Grammar::Grammar( string Vt, string s, vector<Production> p ) :
 
 bool Grammar::sentence( string sf, string t ) {
     return sf.find_first_not_of( t ) == string::npos;
+}
+
+// TODO: altamente ineficiente.
+vector<string> Grammar::generate( int amount ) {
+    auto isSentence = [this]( string str ) {
+        return Grammar::sentence( str, terminals );
+    };
+
+    set<string> T; //Conjunto das formas sentencias geradas até agora
+    set<string> Tp; //Conjunto das próximas formas sentenciais a serem geradas
+    Tp.insert( start );
+
+    int actual = 0;
+    
+    do {
+        T = Tp;
+        for( const string& str : T )
+            for( Production& p : productions ) {
+                vector<string> v = p.apply( str );
+                Tp.insert( v.begin(), v.end() );
+            }
+        /* Aqui, todas as novas formas sentenciais já foram geradas.
+         * Temos de contar quantas sentenças há entre elas. */
+        actual = count_if( Tp.begin(), Tp.end(), isSentence );
+    } while( actual < amount );
+
+    vector<string> r( actual );
+    copy_if( Tp.begin(), Tp.end(), r.begin(), isSentence );
+    return r;
 }
--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include "production.h"
 #include "parse.h"
 #include "grammar.h"
 
 using std::vector;
 using std::string;
-
+using std::sort;
 using std::cin;
 using std::cout;
 using std::endl;
@@ -34,6 +35,11 @@ int main() {
 
     int q;
     cin >> q;
-    cout << q << endl;
+    vector<string> sentences = g.generate( q );
+
+    sort( sentences.begin(), sentences.end(),
+            [](string i, string j) { return i.length() < j.length(); } );
+    for( string& str: sentences ) //Imprimir em ordem crescente de tamanho
+        cout << str << endl;
     return 0;
 }
